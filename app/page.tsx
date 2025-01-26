602fecce5030a74b95cf7ca0e8d9d8e7b44f9cea
sand14 <avrigeanusebastian@gmail.com>
2025-01-26 02:02:49 +0200

    Remove vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import GoogleMaps from "./components/GoogleMaps";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <>
       <GoogleMaps/>
-      <SpeedInsights/>
-      <Analytics/>
     </>
   );
 }
