4ef39820c5841c081b4bc1286168e523daead3f3
sand14 <sand.avrigeanu@yahoo.com>
2025-04-10 01:40:36 +0300

    chore(deps-dev): Update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
