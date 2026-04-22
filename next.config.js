25477d273b13119e593c4cc62ba11c2085a96ef8
sand14 <sand.avrigeanu@yahoo.com>
2026-04-22 15:26:10 +0300

    fix: update Dockerfile and workflow for improved build process and remove unused environment variable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  },
   output: "standalone",
-  };
+};
